d0655c9b3cd42ed42b70d3186ac2c1ab913cc384
Twelve-3 <reiji0507@gmail.com>
2022-03-08 11:51:20 +0900

    フッターの設定
    2022/03/08

--- sources/common.js
+++ sources/common.js
@@ -8,3 +8,14 @@ function header(){
         }
     });
 }
+
+function footer(){
+    //document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
+    $.ajax({
+        url: "htmls/foot.html",
+        cache: false,
+        success: function(html){
+            document.write(html);
+        }
+    });
+}
