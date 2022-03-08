0b1ad8be51633de3b5a835d8ad186e1da2ca58e2
Twelve-3 <reiji0507@gmail.com>
2022-03-08 10:16:25 +0900

    試行錯誤
    大切だよね。
    2022/03/08

--- sources/common.js
+++ sources/common.js
@@ -1,21 +1,3 @@
-var xhr = new XMLHttpRequest();
-
 function header(){
-    xhr.open("get","htmls/head.html");
-    xhr.send();
-    xhr.addEventListener('readystatechange', () => {
-		if( xhr.readyState === 4 && xhr.status === 200) {
-			
-		}
-	});
-}
-
-function footer(){
-    $.ajax({
-        url:"htmls/foot.html",
-        cache:false,
-        success:function(html){
-            document.write(html);
-        }
-    });
+    
 }
