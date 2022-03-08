e56a3231963316e2fb9c04c8df75d7f8ee295571
Twelve-3 <reiji0507@gmail.com>
2022-03-08 11:49:26 +0900

    jQuery を使用した
    2022/03/08

--- sources/common.js
+++ sources/common.js
@@ -1,3 +1,10 @@
 function header(){
-    document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
+    //document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
+    $.ajax({
+        url: "htmls/head.html",
+        cache: false,
+        success: function(html){
+            document.write(html);
+        }
+    });
 }
