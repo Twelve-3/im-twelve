0dc2d12ba7027c2dd9594b472c29648f22d673d4
Twelve-3 <reiji0507@gmail.com>
2022-03-08 17:46:54 +0900

    リンク強化

--- sources/common.js
+++ sources/common.js
@@ -4,3 +4,12 @@ function hf(){
         $("#foot").load("parts/foot.html");
       });
 }
+
+document.getElementById('twitMain').addEventListener('mouseover', function(){
+	document.getElementById('twitMain').innerHTML = '乗った！';
+	document.getElementById('twitMain').style.background = '#cc9900';
+});
+document.getElementById('twitMain').addEventListener('mouseout', function(){
+	document.getElementById('twitMain').innerHTML = '離れた！';
+	document.getElementById('twitMain').style.background = '#0099cc';
+});
