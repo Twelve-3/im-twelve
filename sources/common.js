e82a024ac66480fa6a74d98d75dc4ae56ff70344
Twelve-3 <reiji0507@gmail.com>
2022-03-08 13:28:00 +0900

    外部ファイル化
    JavaScript コードを外部ファイルに移行しただけ
    2022/03/08

--- sources/common.js
+++ sources/common.js
@@ -1,24 +1,6 @@
-
-
-/*
-function header(){
-    //document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
-    $.ajax({
-        url: "htmls/head.html",
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
+function hf(){
+    $(function(){
+        $("#head").load("htmls/head.html");
+        $("#foot").load("htmls/foot.html");
+      });
 }
-
-function footer(){
-    //document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
-    $.ajax({
-        url: "htmls/foot.html",
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
-}*/
