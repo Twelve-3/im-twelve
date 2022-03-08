7b99e91a49877d71796018ad10d361d0b1154454
Twelve-3 <reiji0507@gmail.com>
2022-03-08 10:54:44 +0900

    もろもろ

--- sources/common.js
+++ sources/common.js
@@ -1,3 +1,15 @@
 function header(){
-    
+    var xhr = new XMLHttpRequest(),
+		method = "GET",
+		url = "head.html";//読み込まれるHTMLを指定
+	var hdisp=document.getElementById("headDisp");//読み込みたい位置を指定
+ 
+	xhr.open(method, url, true);
+	xhr.onreadystatechange = function () {
+		if(xhr.readyState === 4 && xhr.status === 200) {
+			var restxt=xhr.responseText;//String型で取得
+			box.innerHTML=restxt;//完了
+		}
+	};
+	xhr.send();
 }
