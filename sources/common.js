1cd87df1d0d085f8ade65e5145b6315c0bd81c42
Twelve-3 <reiji0507@gmail.com>
2022-03-08 11:11:00 +0900

    Update common.js
    2022/03/08

--- sources/common.js
+++ sources/common.js
@@ -1,4 +1,17 @@
 function header(){
     //document.write("<header><h1 class=\"title\"><a>Twelve's website</a></h1></header>");
-    var req = XMLHttpRequest();
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function(){
+        if(req.readyState == 4){
+            if(req.status == 200){
+                var data = req.responseXML;
+                document.write(data);
+            }
+        }
+    }
+
+    req.open("GET","htmls/head.html");
+
+
 }
