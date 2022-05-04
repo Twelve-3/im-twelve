d3c492994d53c171a671f076879a67e58df007e3
Twelve-3 <reiji0507@gmail.com>
2022-05-05 06:48:33 +0900

    誕生日を計算する

--- sources/common.js
+++ sources/common.js
@@ -1,16 +1,31 @@
-window.onload = function(){
-  $(function(){
-  var style = "<link rel=\"stylesheet\" href=\"css/animation.css\">";
-  $("head link:last").after(style);
+window.onload = function () {
+  $(function () {
+    var style = "<link rel=\"stylesheet\" href=\"css/animation.css\">";
+    $("head link:last").after(style);
   });
 }
 
-document.oncontextmenu = function () {return false;}
+document.oncontextmenu = function () { return false; }
 
-function hf(){
-    $(function(){
-        $("#head").load("parts/head.html");
-        $("#foot").load("parts/foot.html");
-      });
-      
+function hf() {
+  $(function () {
+    $("#head").load("parts/head.html");
+    $("#foot").load("parts/foot.html");
+  });
+
 }
+
+/*
+function birthday() {
+  const Year = 2005;
+  const Month = 5;
+  const Day = 7;
+
+  var today = new Date();
+  var nowBirth = new Date(today.getFullYear(), Month-1, Day);
+  var age = today.getFullYear() - Year;
+  if(today < nowBirth){
+    age--;
+  }
+  return age;
+}*/
